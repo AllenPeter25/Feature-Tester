aea99c7bff65caabaa31d1b1ce5215af6c18df99
Allen Peter <allen.peter@netcracker.com>
2023-02-27 00:17:35 +0530

    Added dashboard dynamic cards

--- script/dashboard.js
+++ script/dashboard.js
@@ -11,3 +11,36 @@ document.querySelectorAll(".sidebar__links")[5].addEventListener('click',() => {
     document.querySelector(".sidebar").classList.toggle("sidebar--width")
     document.querySelectorAll(".sidebar__links")[5].childNodes[1].classList.toggle("expand-btn")
 })
+
+fetch('/data.json').then(response => {
+    return response.json();
+  }).then(data => {
+
+    data.forEach((x) => {
+        const parent = document.querySelector(".task-status__data-list")
+        const ele = document.createElement("div")
+        ele.classList.add("task-status__data-list--card")
+        const div = document.createElement("div")
+        const innerDiv = document.createElement("div")
+        innerDiv.classList.add("task-status__data-list--title")
+        const title = document.createElement("p")
+        title.classList.add("task-status__data-list--title--product")
+        title.textContent = x.title
+        const status = document.createElement("p")
+        status.classList.add((x.status == "Completed")?"task-status__data-list--title--completed":"task-status__data-list--title--inprogress")
+        status.textContent = x.status
+        const location = document.createElement("p")
+        location.classList.add("task-status__data-list--title--location")
+        location.innerHTML = `<span class="location">Location:</span> ${x.location}`
+        const name = document.createElement("p")
+        name.classList.add("task-status__data-list--title--location")
+        name.textContent = x.name
+        innerDiv.appendChild(title)
+        innerDiv.appendChild(status)
+        div.appendChild(innerDiv)
+        ele.appendChild(div)
+        ele.appendChild(location)
+        ele.appendChild(name)
+        parent.appendChild(ele)
+    })
+  })
