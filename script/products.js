224ef37379070f6f8b01b2d2447d65eaa309eafd
Allen Peter <allen.peter@netcracker.com>
2023-03-02 10:57:04 +0530

    Products page Completion

--- script/products.js
+++ script/products.js
@@ -10,5 +10,50 @@ document.querySelectorAll(".sidebar__links")[5].addEventListener('click',() => {
     })
     document.querySelector(".sidebar").classList.toggle("sidebar--width")
     document.querySelectorAll(".sidebar__links")[5].childNodes[1].classList.toggle("expand-btn")
-    // document.querySelector(".task-status").classList.toggle("task-status--sidebar-collape")
+    document.querySelector(".content-wrapper").classList.toggle("content-wrapper--sidebar-collape")
+})
+
+const img = document.querySelectorAll('#imgs img');
+
+let idx = 0;
+
+function forward() {
+  idx++;
+
+  if (idx > img.length - 1) {
+    idx = 0;
+  }
+
+  imgs.style.transform = `translateX(${-idx * 590}px)`;
+}
+
+function backward() {
+    idx--;
+  
+    if (idx < img.length - 1) {
+      idx = 0;
+    }
+  
+    imgs.style.transform = `translateX(${idx * 590}px)`;
+  }
+
+document.querySelector(".carousel__next").addEventListener('click', () =>{
+    forward()
+})
+
+document.querySelector(".carousel__previous").addEventListener('click', () => {
+    backward()
+})
+
+fetch('/data/products.json').then(response => {
+  return response.json()
+}).then(data => {
+  data.forEach((x) => {
+    const parent = document.querySelector(".products-list")
+    const cardHTML = `<div class="products-list__card">
+    <img src="${x.picture}">
+    <p>${x.device}</p>
+    <div>`
+    parent.innerHTML += cardHTML
+  })
 })
